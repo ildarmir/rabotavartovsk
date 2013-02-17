6f41bb6644e96dd459248c772b03e13d457a4a80
nostsaber <nostsaber@gmail.com>
2013-02-17 21:42:11 +0600

    Locales

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,7 @@
 class IndexController < ApplicationController
   def index
     @vac = Vacancy.limit(5).order("id DESC")
-    @res = Resume.limit(5).order("id DESC")
-    flash[:notice] = t(:hello_flash)
+    @res = Resume.limit(5).order("id DESC")    
   end
 
 end
