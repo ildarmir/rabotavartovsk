fa285fbdc92761b7731f7b0e13d67c97b883845b
ildar <ildar@debian.ufanet.ru>
2013-04-14 12:58:16 +0600

    first commit on new branch

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,5 @@
 class Resume < ActiveRecord::Base
+belongs_to :user
   
   attr_accessible :about, :age, :avatar, :city, :date, :education, :email, :experience, :group_id,  :mar_status, :min_wage, :name, :phone, :position, :schedule, :sex, :skills, :view
   
--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,5 @@
 class Vacancy < ActiveRecord::Base
+belongs_to :user
   attr_accessible :date, :email, :experience, :note, :organisation, :phone, :position, :salary
   
   validates :position, :presence => true
