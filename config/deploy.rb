c792d52ead6eaa31632df169774a0396c7d70a12
ildarmir <fantomasu@yahoo.com>
2013-05-13 19:41:36 +0600

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'bundler/capistrano'
-after "deploy:update_code", :copy_database_config, :copy_production_config
+before "deploy:update_code", :copy_database_config, :copy_production_config
 task :copy_database_config, roles => :app do
   db_config = "#{shared_path}/database.yml"
   run "cp #{db_config} #{release_path}/config/database.yml"
