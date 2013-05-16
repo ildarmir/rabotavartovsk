63daf6858af91de138b0c546bd9cb013899fb153
ildarmir <fantomasu@yahoo.com>
2013-05-16 18:17:52 +0600

    add to password recovery page login recovery and add new yahoo mail accoun to development

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,8 +34,12 @@ end
   end
 # assign them a random one and mail it to them, asking them to change it
     def forgot_password
+    if params[:mail]!=nil
         @user = User.find_by_mail(params[:mail])
-	    if @user
+    elsif params[:name]!=nil
+		@user=User.find_by_name(params[:name])
+    end
+	    if @user && @user.mail!=nil
 	    chars = ('a'..'z').to_a + ('1'..'9').to_a - ['o', 'O', 'i', 'I']  	    
 	    random_password = Array.new(7) { chars[rand(chars.size)] }.join
 	        @user.password = random_password
@@ -46,7 +50,7 @@ end
   if @user
         format.html { redirect_to  login_url, notice: "Проверьте почтовый ящик" }
         format.json { render json: @user, status: :created, location: @user }
-      else
+      elsif @user.nil?
         format.html { render action: "new", notice: "Почтовый ящик не зарегистрирован" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
   end
