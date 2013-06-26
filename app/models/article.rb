ee82956e913c99f366dfa44e57bbe6a8b5611045
ildarmir <fantomasu@yahoo.com>
2013-06-26 23:52:58 +0600

    move seo to sidebar in mainpage layout, change articles show and index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
   attr_accessible :add_time, :author, :des, :mini_img, :text, :title, :view, :as=>:admin
   attr_accessible :add_time, :author, :des, :mini_img, :text, :title, :view
+  mount_uploader :mini_img, ArticleImageUploader
 end
