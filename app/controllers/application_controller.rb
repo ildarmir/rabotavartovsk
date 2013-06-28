5911b008225a706d62b3f44e37f8764c3ef94adf
ildarmir <fantomasu@yahoo.com>
2013-06-28 15:10:15 +0600

    change view article in sidebar, now random article add to sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,29 @@
 # coding: utf-8
 class ApplicationController < ActionController::Base
-        layout "mainpage"
- 	before_filter :set_locale
-	def set_locale
-	  I18n.locale = params[:locale] || I18n.default_locale
-	end
-        def default_url_options(options={})
-	  logger.debug "default_url_options is passed options: #{options.inspect}\n"
-	  { :locale => I18n.locale }
-	end
-        before_filter :authorize 
-        protected
-        def authorize
-          unless User.find_by_id(session[:user_id])
-          redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
-	  else
-	@user=User.find_by_id(session[:user_id])
-        end
-	end
-
-
-	
-
+  layout "mainpage"
   protect_from_forgery
+  before_filter :set_locale, :get_article
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def get_article
+    idarr=Article.select("id").map {|k| k.id}
+    id=idarr[rand(idarr.size)]
+    @side_article=Article.find(id)
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+      { :locale => I18n.locale }
+  end
+  before_filter :authorize 
+  protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+    else
+      @user=User.find_by_id(session[:user_id])
+    end
+  end
 end
