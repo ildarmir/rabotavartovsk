55e113961f65737e74e38444981f7bb23f81743e
ildarmir <fantomasu@yahoo.com>
2013-07-10 17:19:49 +0600

    add advertise to bottom of page, add views to vacancies#show

--- app/assets/javascripts/vacancies.js
+++ app/assets/javascripts/vacancies.js
@@ -1,5 +1,6 @@
 addEventListener("load", LoadWindow);
 function LoadWindow () {
+      $('.carousel').carousel({interval: 8000});
     var input = document.getElementById('quickSearchQuery');
     var searchInput = new InputFilter({elem: input, filtered: "#vacancy_search #vacancy_table_result .vacancy_table_result_left"});
     addEventListener("keyup", function () { searchInput.filter() }  );
