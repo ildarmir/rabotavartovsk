f59e7049424548257fed39471e22257102a2eb3b
ildarmir <fantomasu@yahoo.com>
2013-07-19 14:02:23 +0600

    change mainpage text, best resumes other order

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,13 +1,13 @@
 class MainpageController < ApplicationController
   skip_before_filter :authorize
  def index
-    @vac = Vacancy.limit(5).order("created_at DESC")
-    pop = Vacancy.select("id,position,view").order("created_at desc").limit(25)
+    @vac = Vacancy.limit(8).order("created_at DESC")
+    pop = Vacancy.select("id,position,view").order("created_at desc").limit(30)
     pop=pop.each {|k| k.view=0 if k.view.nil? }
     pop.sort_by! {|k| k.view}
-    @pop=pop.reverse.slice(0..4)
-    @res = Resume.limit(5).order("id DESC")    
-    @best = Resume.where("avatar is not null").limit(6).order("id DESC")
+    @pop=pop.reverse.slice(0..7)
+    @res = Resume.limit(7).order("id DESC")    
+    @best = Resume.where("avatar is not null and education!='' and skills!=''  and about!='' and email!='' and phone!=''").limit(2).order("id DESC")
  end
   
 end
