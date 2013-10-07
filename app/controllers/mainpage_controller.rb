d11055446994bf5f68e79e12fc1c911eeea04902
i <i@pc>
2013-10-08 03:30:24 +0600

    change mainpage

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,11 +1,11 @@
 class MainpageController < ApplicationController
   skip_before_filter :authorize
  def index
-    @vac = @city.vacancies.limit(8).order("created_at DESC")
-    pop = @city.vacancies.select("id,position,view").order("created_at desc").limit(30)
+    @vac = @city.vacancies.limit(6).order("created_at DESC")
+    pop = @city.vacancies.select("id,position,view,organisation").order("created_at desc").limit(30)
     pop=pop.each {|k| k.view=0 if k.view.nil? }
     pop.sort_by! {|k| k.view}
-    @pop=pop.reverse.slice(0..7)
+    @pop=pop.reverse.slice(0..5)
     @res = @city.resumes.limit(7).order("id DESC")    
     @best = @city.resumes.where("avatar is not null and education!='' and skills!=''  and about!='' and email!='' and phone!=''").limit(2).order("id DESC")
  end
